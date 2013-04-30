1a0191fca9a29ab8814b19e4997e764ee1902bfb
Martin Holman <martin@raygun.io>
2013-05-01 10:06:45 +1200

    update version, buildfile etx

--- dist/raygun.js
+++ dist/raygun.js
@@ -1,4 +1,4 @@
-/*! Raygun4js - v1.2.1 - 2013-04-22
+/*! Raygun4js - v1.3.0 - 2013-05-01
 * https://github.com/MindscapeHQ/raygun4js
 * Copyright (c) 2013 MindscapeHQ; Licensed MIT */
 ;(function(window, undefined) {
@@ -104,6 +104,7 @@ TraceKit.report = (function reportModuleWrapper() {
      * @param {Function} handler
      */
     function subscribe(handler) {
+        installGlobalHandler();
         handlers.push(handler);
     }
 
@@ -143,7 +144,7 @@ TraceKit.report = (function reportModuleWrapper() {
         }
     }
 
-    var _oldOnerrorHandler = window.onerror;
+    var _oldOnerrorHandler, _onErrorHandlerInstalled;
 
     /**
      * Ensures all global unhandled exceptions are recorded.
@@ -153,7 +154,7 @@ TraceKit.report = (function reportModuleWrapper() {
      * @param {(number|string)} lineNo The line number at which the error
      * occurred.
      */
-    window.onerror = function traceKitWindowOnError(message, url, lineNo) {
+    function traceKitWindowOnError(message, url, lineNo) {
         var stack = null;
 
         if (lastExceptionStack) {
@@ -184,7 +185,17 @@ TraceKit.report = (function reportModuleWrapper() {
         }
 
         return false;
-    };
+    }
+
+    function installGlobalHandler ()
+    {
+        if (_onErrorHandlerInstalled === true) {
+            return;
+        }
+        _oldOnerrorHandler = window.onerror;
+        window.onerror = traceKitWindowOnError;
+        _onErrorHandlerInstalled = true;
+    }
 
     /**
      * Reports an unhandled Error to TraceKit.
@@ -1081,67 +1092,6 @@ TraceKit.computeStackTrace = (function computeStackTraceWrapper() {
     _helper('setInterval');
 }());
 
-/**
- * Extended support for backtraces and global error handling for most
- * asynchronous jQuery functions.
- */
-(function traceKitAsyncForjQuery($) {
-
-    // quit if jQuery isn't on the page
-    if (!$) {
-        return;
-    }
-
-    var _oldEventAdd = $.event.add;
-    $.event.add = function traceKitEventAdd(elem, types, handler, data, selector) {
-        var _handler;
-
-        if (handler.handler) {
-            _handler = handler.handler;
-            handler.handler = TraceKit.wrap(handler.handler);
-        } else {
-            _handler = handler;
-            handler = TraceKit.wrap(handler);
-        }
-
-        // If the handler we are attaching doesn’t have the same guid as
-        // the original, it will never be removed when someone tries to
-        // unbind the original function later. Technically as a result of
-        // this our guids are no longer globally unique, but whatever, that
-        // never hurt anybody RIGHT?!
-        if (_handler.guid) {
-            handler.guid = _handler.guid;
-        } else {
-            handler.guid = _handler.guid = $.guid++;
-        }
-
-        return _oldEventAdd.call(this, elem, types, handler, data, selector);
-    };
-
-    var _oldReady = $.fn.ready;
-    $.fn.ready = function traceKitjQueryReadyWrapper(fn) {
-        return _oldReady.call(this, TraceKit.wrap(fn));
-    };
-
-    var _oldAjax = $.ajax;
-    $.ajax = function traceKitAjaxWrapper(s) {
-        var keys = ['complete', 'error', 'success'], key;
-        while(key = keys.pop()) {
-            if ($.isFunction(s[key])) {
-                s[key] = TraceKit.wrap(s[key]);
-            }
-        }
-
-        try {
-            return _oldAjax.call(this, s);
-        } catch (e) {
-            TraceKit.report(e);
-            throw e;
-        }
-    };
-
-}(window.jQuery));
-
 //Default options:
 if (!TraceKit.remoteFetching) {
   TraceKit.remoteFetching = true;
@@ -1160,6 +1110,63 @@ if (!TraceKit.linesOfContext || TraceKit.linesOfContext < 1) {
 window.TraceKit = TraceKit;
 
 }(window));
+
+(function traceKitAsyncForjQuery($, TraceKit) {
+  'use strict';
+  // quit if jQuery isn't on the page
+  if (!$) {
+    return;
+  }
+
+  var _oldEventAdd = $.event.add;
+  $.event.add = function traceKitEventAdd(elem, types, handler, data, selector) {
+    var _handler;
+
+    if (handler.handler) {
+      _handler = handler.handler;
+      handler.handler = TraceKit.wrap(handler.handler);
+    } else {
+      _handler = handler;
+      handler = TraceKit.wrap(handler);
+    }
+
+    // If the handler we are attaching doesn’t have the same guid as
+    // the original, it will never be removed when someone tries to
+    // unbind the original function later. Technically as a result of
+    // this our guids are no longer globally unique, but whatever, that
+    // never hurt anybody RIGHT?!
+    if (_handler.guid) {
+      handler.guid = _handler.guid;
+    } else {
+      handler.guid = _handler.guid = $.guid++;
+    }
+
+    return _oldEventAdd.call(this, elem, types, handler, data, selector);
+  };
+
+  var _oldReady = $.fn.ready;
+  $.fn.ready = function traceKitjQueryReadyWrapper(fn) {
+    return _oldReady.call(this, TraceKit.wrap(fn));
+  };
+
+  var _oldAjax = $.ajax;
+  $.ajax = function traceKitAjaxWrapper(s) {
+    var keys = ['complete', 'error', 'success'], key;
+    while(key = keys.pop()) {
+      if ($.isFunction(s[key])) {
+        s[key] = TraceKit.wrap(s[key]);
+      }
+    }
+
+    try {
+      return _oldAjax.call(this, s);
+    } catch (e) {
+      TraceKit.report(e);
+      throw e;
+    }
+  };
+
+}(window.jQuery, window.TraceKit));
 
 (function (window, $) {
   // pull local copy of TraceKit to handle stack trace collection
