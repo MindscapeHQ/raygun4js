249d6a245121c3dcebc2dbb99de96ca7a8f7da21
fundead <callum@mindscape.co.nz>
2015-05-01 13:14:49 +1200

    Ajax errors: add the activeTarget markup to the customdata

--- src/raygun.js
+++ src/raygun.js
@@ -302,14 +302,16 @@ var raygunFactory = function (window, $, undefined) {
     }
 
     Raygun.send(thrownError || event.type, {
-      status: jqXHR.status,
-      statusText: jqXHR.statusText,
-      type: ajaxSettings.type,
-      url: ajaxSettings.url,
-      ajaxErrorMessage: message,
-      contentType: ajaxSettings.contentType,
-      requestData: ajaxSettings.data && ajaxSettings.data.slice ? ajaxSettings.data.slice(0, 10240) : undefined,
-      responseData: jqXHR.responseText && jqXHR.responseText.slice ? jqXHR.responseText.slice(0, 10240) : undefined });
+        status: jqXHR.status,
+        statusText: jqXHR.statusText,
+        type: ajaxSettings.type,
+        url: ajaxSettings.url,
+        ajaxErrorMessage: message,
+        contentType: ajaxSettings.contentType,
+        requestData: ajaxSettings.data && ajaxSettings.data.slice ? ajaxSettings.data.slice(0, 10240) : undefined,
+        responseData: jqXHR.responseText && jqXHR.responseText.slice ? jqXHR.responseText.slice(0, 10240) : undefined,
+        activeTarget: event.target && event.target.activeElement ? event.target.activeElement.outerHTML : undefined
+      });
   }
 
 
