1b6ee9fc7d5d2b6f403171ebb61524ca13e7ae5b
Jeremy <jeremy@mindscape.co.nz>
2015-11-03 17:04:53 +1300

    Handle malformed cookie - return null

--- src/raygun.rum.js
+++ src/raygun.rum.js
@@ -218,17 +218,22 @@ var raygunRumFactory = function (window, $, Raygun) {
         }
 
         function readCookie(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) === ' ') {
-                    c = c.substring(1, c.length);
+            try {
+                var nameEQ = name + "=";
+                var ca = document.cookie.split(';');
+                for (var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) === ' ') {
+                        c = c.substring(1, c.length);
+                    }
+                    if (c.indexOf(nameEQ) === 0) {
+                        return c.substring(nameEQ.length, c.length);
+                    }
                 }
-                if (c.indexOf(nameEQ) === 0) {
-                    return c.substring(nameEQ.length, c.length);
-                }
+            } 
+            catch (e) {                
             }
+
             return null;
         }
 
