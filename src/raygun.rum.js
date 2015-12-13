f5825c4732a54a8eef9bdc29cc187ba1621cc178
fundead <callum@mindscape.co.nz>
2015-12-14 11:49:21 +1300

    WIP

--- src/raygun.rum.js
+++ src/raygun.rum.js
@@ -336,9 +336,9 @@ var raygunRumFactory = function (window, $, Raygun) {
 
         function generateVirtualEncodedTimingData() {
           return {
-            t: 'p', a: 0, b: 0, c: 0, d: null, e: null,
+            t: 'v', a: 0, b: 0, c: 0, d: null, e: null,
             f: 0, g: 0, h: 0, i: 0,
-            j: 0, k: 0, l: null, m: null, n: null
+            j: 0, k: 0, l: null, m: null, n: null, o: 9999
           };
         }
 
