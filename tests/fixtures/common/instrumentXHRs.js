721c5059323db9daf49f362e64efe1c35dc86ff0
fundead <callum@mindscape.co.nz>
2016-12-20 11:25:56 +1300

    Instrument XHR to capture outgoing request body for payload E2E validation tests

--- tests/fixtures/common/instrumentXHRs.js
+++ tests/fixtures/common/instrumentXHRs.js
@@ -1,12 +1,16 @@
 (function() {
+  window.__requestBodies = [];
   window.__inFlightXHRs = [];
   window.__completedXHRs = [];
 
   var origOpen = XMLHttpRequest.prototype.open;
+  var origSend = XMLHttpRequest.prototype.send;
+
   XMLHttpRequest.prototype.open = function() {
 
     window.__inFlightXHRs.push({
       xhr: this,
+      orig: origOpen,
       method: arguments[0],
       url: arguments[1]
     });
@@ -17,4 +21,13 @@
 
     origOpen.apply(this, arguments);
   };
+
+  XMLHttpRequest.prototype.send = function() {
+    window.__requestBodies.push({
+      self: this,
+      args: arguments
+    });
+
+    origSend.apply(this, arguments);
+  }
 })()
---
+++ tests/specs/v1/payloadTests.js
@@ -0,0 +1,24 @@
+var webdriverio = require('webdriverio');
+var _ = require('underscore');
+
+describe("Payload functional validation tests for V1", function() {
+  
+  it("has the error message in the payload set", function () {
+    browser.url('http://localhost:4567/fixtures/v1/manualSend.html');
+
+    browser.pause(4000);
+
+    var requestBodies = browser.execute(function () {
+      return window.__requestBodies;
+    });
+
+    debugger;
+
+    var hasRequest = _.any(requestBodies.value, function (req) {
+      return req.args;
+    });
+
+    expect(hasRequest).toBe(false);
+  });
+
+});
