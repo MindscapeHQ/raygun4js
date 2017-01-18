9361c8d8e08312bdebc53d3e555f41e7e3a25f04
fundead <callum@mindscape.co.nz>
2017-01-18 17:01:45 +1300

    UMD intro templated

--- src/umd.intro.js
+++ src/umd.intro.js
@@ -3,16 +3,16 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['b'], function (b) {
-            return (root.returnExportsGlobal = factory(b));
+        define(['raygun4js'], function (raygun4js) {
+            return (root.Raygun = factory(raygun4js));
         });
     } else if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        module.exports = factory(require('b'));
+        module.exports = factory(require('raygun4js'));
     } else {
         // Browser globals
-        root.returnExportsGlobal = factory(root.b);
+        root.Raygun = factory(root.raygun4js);
     }
-}(this, function (b) {
+}(this, function (raygun4js) {
