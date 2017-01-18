716b2260fefd784ae3064c8e145890597da702c4
fundead <callum@mindscape.co.nz>
2017-01-18 21:18:49 +1300

    Candidate which exports rg4js as the result of the UMD module, may be too hacky in practice despite unified API

--- src/umd.intro.js
+++ src/umd.intro.js
@@ -16,3 +16,12 @@
         root.Raygun = factory(root.raygun4js);
     }
 }(this, function (raygun4js) {
+
+  var windw = this;
+
+  // Same approach as the snippet, creates the rg4js proxy function, which is exported in umd.outro.js once the
+  // script is executed
+  (function(wind) { wind['RaygunObject'] = 'rg4js';
+  wind['rg4js'] = wind['rg4js'] || function() {
+      (wind['rg4js'].o = wind['rg4js'].o || []).push(arguments)
+  }})(windw);
