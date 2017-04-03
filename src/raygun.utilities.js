793373115c45d9fc49b81cc4228966c12f9402ec
Taylor Lodge <ubermouse894@gmail.com>
2017-04-04 10:15:33 +1200

    Add polyfills for Array.map and Array.forEach

--- src/raygun.utilities.js
+++ src/raygun.utilities.js
@@ -51,6 +51,84 @@ var raygunUtilityFactory = function (window) {
     };
   }
 
+  // Production steps of ECMA-262, Edition 5, 15.4.4.19
+  // Reference: http://es5.github.io/#x15.4.4.19
+  if (!Array.prototype.map) {
+
+    Array.prototype.map = function(callback/*, thisArg*/) {
+
+      var T, A, k;
+
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+
+      if (arguments.length > 1) {
+        T = arguments[1];
+      }
+
+      A = new Array(len);
+      k = 0;
+
+      while (k < len) {
+        var kValue, mappedValue;
+
+        if (k in O) {
+          kValue = O[k];
+
+          mappedValue = callback.call(T, kValue, k, O);
+          A[k] = mappedValue;
+        }
+        k++;
+      }
+
+      return A;
+    };
+  }
+
+  // Production steps of ECMA-262, Edition 5, 15.4.4.18
+  // Reference: http://es5.github.io/#x15.4.4.18
+  if (!Array.prototype.forEach) {
+
+    Array.prototype.forEach = function(callback/*, thisArg*/) {
+      var T, k;
+
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+
+      if (arguments.length > 1) {
+        T = arguments[1];
+      }
+
+      k = 0;
+      while (k < len) {
+        var kValue;
+
+        if (k in O) {
+          kValue = O[k];
+
+          callback.call(T, kValue, k, O);
+        }
+        k++;
+      }
+    };
+  }
+
   // Mozilla's bind() shim for IE8
   if (!Function.prototype.bind) {
       Function.prototype.bind = function (oThis) {
