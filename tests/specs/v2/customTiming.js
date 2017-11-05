6a7b8fd8c82c919d368d4b46361f38dc5be19af1
Taylor Lodge <ubermouse894@gmail.com>
2017-11-06 11:03:36 +1300

    Add test covering sending custom timing data

---
+++ tests/specs/v2/customTiming.js
@@ -0,0 +1,25 @@
+var webdriverio = require('webdriverio');
+
+describe("Custom Timing tests", function() {
+
+  // Setup
+
+  beforeEach(function() {
+    browser.url('http://localhost:4567/fixtures/v2/customTiming.html');
+
+    browser.pause(1000);
+  });
+
+  // Tests
+
+  it('sends custom timing events', function () {
+    var customTimingData = browser.execute(function () {
+      return window.__requestPayloads[1];
+    }).value;
+
+    expect(JSON.parse(customTimingData.eventData[0].data)[0].customTiming).toEqual({
+      custom1: 100,
+      custom2: 50
+    });
+  });
+});
