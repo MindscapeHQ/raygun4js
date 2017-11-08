ddf67ce113adbf1eb49571d78436d0dd40d8062c
Taylor Lodge <ubermouse894@gmail.com>
2017-11-09 09:48:57 +1300

    Update withTags option to handle plain strings
    This isn't inherently supported, but it was silently causing a failure
    only in the specific case of unhandled exceptions. Just fixing that
    specific case was the easiest way to resolve that crash.

--- tests/specs/v2/payloadUnhandledErrorTests.js
+++ tests/specs/v2/payloadUnhandledErrorTests.js
@@ -206,4 +206,24 @@ describe("Payload functional validation tests for v2 automatic unhandled error s
     expect(passes).toBe(true);
   });
 
+  it("has existing tags and an UnhandleException tag for crash vs. error support when using a function for withTags", function () {
+    var pageUrl = 'http://localhost:4567/fixtures/v2/unhandledErrorTagWithString.html';
+
+    browser.url(pageUrl);
+
+    browser.pause(4000);
+
+    var requestPayloads = browser.execute(function () {
+      return window.__requestPayloads;
+    });
+
+    var passes = _.any(requestPayloads.value, function (payload) {
+      return _.any(payload.Details.Tags, function (tag) {
+        return tag === 'UnhandledException';
+      });
+    });
+
+    expect(passes).toBe(true);
+  });
+
 });
