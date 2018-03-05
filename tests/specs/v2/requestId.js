bd72d398fe3dacad795d0658df5accd59c54f65d
samuel-holt <sam@raygun.com>
2018-03-06 10:51:34 +1300

    Add test for request ID uniqueness

---
+++ tests/specs/v2/requestId.js
@@ -0,0 +1,21 @@
+var webdriverio = require('webdriverio');
+
+describe("Request ID tests", function() {
+
+  // Tests
+
+  it('has a unique request ID for each virtual page request', function() {
+    browser.url('http://localhost:4567/fixtures/v2/requestId.html');
+
+    browser.pause(4000);
+
+    var requestPayloads = browser.execute(function () {
+      return window.__requestPayloads;
+    }).value;
+
+    var requestId1 = requestPayloads[1].eventData[0].requestId;
+    var requestId2 = requestPayloads[2].eventData[0].requestId;
+
+    expect(requestId1).not.toMatch(requestId2);
+  });
+});
