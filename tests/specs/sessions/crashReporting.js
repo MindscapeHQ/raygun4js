21dfa87b3f74531755fad572aed3b622f523d03c
BenjaminHarding <benjamin.p.harding@gmail.com>
2019-03-28 16:58:52 +1300

    Add RUM session storage tests

--- tests/specs/sessions/crashReporting.js
+++ tests/specs/sessions/crashReporting.js
@@ -2,7 +2,6 @@ describe("Crash Reporting Anonymous User Tracking", function() {
   afterEach(function() {
     browser.execute(function() {
       localStorage.clear();
-      sessionStorage.clear();
     });
   });
 
@@ -10,60 +9,31 @@ describe("Crash Reporting Anonymous User Tracking", function() {
     it("creates a user id and persists it in localStorage when one doesn't exist", function() {
       browser.url("http://localhost:4567/fixtures/sessions/crWithoutUser.html");
 
-      var result = browser.execute(function () {
-        return localStorage.getItem("raygun4js-userid") !== null;
-      }).value;
+      var result = common.getLocalStorageValue("raygun4js-userid");
 
-      expect(result).toBe(true);
+      expect(result).not.toBe(null);
     });
 
     it("retrieves user id from localStorage when one exist", function() {
       browser.execute(function() {
-        localStorage.setItem('raygun4js-userid', '1');
+        localStorage.setItem('raygun4js-userid', 'abc123');
       });
 
       browser.url("http://localhost:4567/fixtures/sessions/crWithoutUser.html");
 
-      var result = browser.execute(function () {
-        return localStorage.getItem("raygun4js-userid");
-      }).value;
-
-      expect(result).toBe('1');
+      var result = common.getLocalStorageValue("raygun4js-userid");
+      expect(result).toBe('abc123');
     });
 
     it("retrieves user id from a cookie and sets it in localStorage", function() {
-      browser.execute(function() {
-        var cookieName = 'raygun4js-userid';
-        var cookieValue = '1';
-        document.cookie = cookieName + '=' + cookieValue + '; path=/';
-      });
+      common.setCookieValue('raygun4js-userid', 'xyz789');
 
       browser.url("http://localhost:4567/fixtures/sessions/crWithoutUser.html");
 
-      var localStorageResult = browser.execute(function () {
-        return localStorage.getItem("raygun4js-userid");
-      }).value;
-      expect(localStorageResult).toBe('1');
-
-      var cookieResult = browser.execute(function() {
-        function getCookie(name) {
-          var nameEQ = name + '=';
-          var ca = document.cookie.split(';');
-          for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') {
-              c = c.substring(1, c.length);
-            }
-            if (c.indexOf(nameEQ) === 0) {
-              return c.substring(nameEQ.length, c.length);
-            }
-          }
-          return null;
-        }
-
-        return getCookie('raygun4js-userid');        
-      }).value;
-      expect(cookieResult).toBeFalsy();
+      var result = common.getLocalStorageValue("raygun4js-userid");
+      
+      expect(localStorageResult).toBe('xyz789');
+      expect(common.getCookieValue('raygun4js-userid')).toBeFalsy();
     });
   });
 
@@ -71,10 +41,7 @@ describe("Crash Reporting Anonymous User Tracking", function() {
     it("doesn't set a user id", function() {
       browser.url("http://localhost:4567/fixtures/sessions/crWithUser.html");
 
-      var result = browser.execute(function () {
-        return localStorage.getItem("raygun4js-userid");
-      }).value;
-
+      var result = common.getLocalStorageValue("raygun4js-userid");
       expect(result).toBe(null);
     });
   });
