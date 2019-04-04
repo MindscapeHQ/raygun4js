b8b135efd3fc8ca1f3f285d48113a24ee30bb3d1
Taylor Lodge <ubermouse894@gmail.com>
2019-04-04 16:49:00 +1300

    tests(rum): fix/refactor rum status code tracking tests
    Create shared function to run the tests, the it blocks only load a
    different url and have different timings
    Reload the browser after each test to ensure the session is cleared.
    Otherwise the first test sends a session_start event and the other two
    do not, throwing off the requestPayloads order
    Sort the urls before checking the pairs to ensure the foo url is always
    first, regardless of network timing

--- tests/specs/v2/rumXhrStatusTracking.js
+++ tests/specs/v2/rumXhrStatusTracking.js
@@ -1,20 +1,21 @@
-/* globals describe, it, expect, browser, window, fail */
+/* globals describe, it, expect, browser, window, afterEach, fail */
 
 var _ = require('underscore');
 
 describe("RUM status code tracking", function() {
 
+  afterEach(function() {
+    browser.reload();
+  });
+
   function payloadsWithoutRaygunApi(payloads) {
-    return _.filter(payloads, function(payload) {
+    return _.sortBy(_.filter(payloads, function(payload) {
       return payload.url.indexOf('raygun') === -1;
-    });
+    }), function(payload) { return payload.url; });
   }
 
-  it("attaches the status codes to xhr calls for XmlHttpRequest", function () {
-    browser.url('http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html');
 
-    browser.pause(30000);
-
+  function checkStatusCodes() {
     var requestPayloads = browser.execute(function () {
       return window.__requestPayloads;
     }).value;
@@ -25,23 +26,33 @@ describe("RUM status code tracking", function() {
 
     var timingPayload = payloadsWithoutRaygunApi(JSON.parse(requestPayloads[2].eventData[0].data));
 
-    var pairs = [
-      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'plan relative url'},
+    var expectedPairs = [
+      {url: 'http://localhost:4567/fixtures/v2/foo.html', status: 404, type: 'relative url that does not exist'},
+      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'plain relative url'},
       {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'relative url with query string'},
       {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'absolute url'},
-      {url: 'http://localhost:4567/fixtures/v2/foo.html', status: 404, type: 'relative url that does not exist'},
     ];
 
-    for (var i = 0;i < pairs.length; i++) {
+    console.log(timingPayload);
+    for (var i = 0;i < expectedPairs.length; i++) {
       var payloadUrl = timingPayload[i].url;
       var payloadStatus = timingPayload[i].status;
-      var pairUrl = pairs[i].url;
-      var pairStatus = pairs[i].status;
-      var pairType = pairs[i].type;
+      var pairUrl = expectedPairs[i].url;
+      var pairStatus = expectedPairs[i].status;
+      var pairType = expectedPairs[i].type;
 
       expect(payloadUrl).toBe(pairUrl, "failed for type: " + pairType);
       expect(payloadStatus).toBe(pairStatus, "failed for type: " + pairType);
     }
+  }
+
+
+  it("attaches the status codes to xhr calls for XmlHttpRequest", function () {
+    browser.url('http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html');
+
+    browser.pause(35000);
+
+    checkStatusCodes();
   });
 
   it("attaches status codes to requests for fetch requests", function() {
@@ -57,68 +68,16 @@ describe("RUM status code tracking", function() {
       return;
     }
 
-    browser.pause(29000);
+    browser.pause(34000);
 
-    var requestPayloads = browser.execute(function () {
-      return window.__requestPayloads;
-    }).value;
-
-    if (requestPayloads.length < 2) {
-      fail("test did not wait long enough for ajax requests to be sent to Raygun");
-    }
-
-    var timingPayload = payloadsWithoutRaygunApi(JSON.parse(requestPayloads[1].eventData[0].data));
-
-    var pairs = [
-      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'plan relative url'},
-      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'relative url with query string'},
-      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'absolute url'},
-      {url: 'http://localhost:4567/fixtures/v2/foo.html', status: 404, type: 'relative url that does not exist'},
-    ];
-
-    for (var i = 0;i < pairs.length; i++) {
-      var payloadUrl = timingPayload[i].url;
-      var payloadStatus = timingPayload[i].status;
-      var pairUrl = pairs[i].url;
-      var pairStatus = pairs[i].status;
-      var pairType = pairs[i].type;
-
-      expect(payloadUrl).toBe(pairUrl, "failed for type: " + pairType);
-      expect(payloadStatus).toBe(pairStatus, "failed for type: " + pairType);
-    }
+    checkStatusCodes();
   });
 
   it("attaches the status codes for polyfilled fetch requests", function() {
     browser.url('http://localhost:4567/fixtures/v2/rumFetchPolyfillStatusCodes.html');
 
-    browser.pause(30000);
+    browser.pause(35000);
 
-    var requestPayloads = browser.execute(function () {
-      return window.__requestPayloads;
-    }).value;
-
-    if (requestPayloads.length < 2) {
-      fail("test did not wait long enough for ajax requests to be sent to Raygun");
-    }
-
-    var timingPayload = payloadsWithoutRaygunApi(JSON.parse(requestPayloads[1].eventData[0].data));
-
-    var pairs = [
-      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'plan relative url'},
-      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'relative url with query string'},
-      {url: 'http://localhost:4567/fixtures/v2/rumXhrStatusCodes.html', status: 200, type: 'absolute url'},
-      {url: 'http://localhost:4567/fixtures/v2/foo.html', status: 404, type: 'relative url that does not exist'},
-    ];
-
-    for (var i = 0;i < pairs.length; i++) {
-      var payloadUrl = timingPayload[i].url;
-      var payloadStatus = timingPayload[i].status;
-      var pairUrl = pairs[i].url;
-      var pairStatus = pairs[i].status;
-      var pairType = pairs[i].type;
-
-      expect(payloadUrl).toBe(pairUrl, "failed for type: " + pairType);
-      expect(payloadStatus).toBe(pairStatus, "failed for type: " + pairType);
-    }
+    checkStatusCodes();
   });
 });
