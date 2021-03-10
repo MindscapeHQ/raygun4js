81747a16ad5630aaac501a9c1bf696866ea9c6c0
Krishna Kapadia <kapadikris@myvuw.ac.nz>
2021-03-10 15:44:24 +1300

    Feat: updated vendor to use an IIFE

--- src/raygun.rum/core-web-vitals.js
+++ src/raygun.rum/core-web-vitals.js
@@ -31,13 +31,22 @@ function raygunCoreWebVitalFactory(window) {
         queueTimings = queueHandler;
 
         if(window.webVitals) {
-            window.webVitals.getLCP(this.handler);
-            window.webVitals.getFID(this.handler);
-            window.webVitals.getCLS(this.handler);
+            if(window.webVitals.getLCP) {
+                window.webVitals.getLCP(this.handler);
+            }
+
+            if(window.webVitals.getFID) {
+                window.webVitals.getFID(this.handler);
+            }
+
+            if(window.webVitals.getCLS) {
+                window.webVitals.getCLS(this.handler);
+            }
         }
     };
 
     CoreWebVitals.prototype.handler = function(event) {
+        window.console.log(event);
         if(event.value && event.value.toFixed) {
             event.value = event.value.toFixed(3);
         }
