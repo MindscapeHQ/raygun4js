8f22c29fe917a44187cf9cf57e45cd983061cc4b
Darcy Thomas <dthomas@raygun.com>
2023-07-11 15:31:47 +1200

    Update payloadManualSendTests.js

--- tests/specs/v1/payloadManualSendTests.js
+++ tests/specs/v1/payloadManualSendTests.js
@@ -7,7 +7,7 @@ var _entriesEndpoint = 'https://api.raygun.io/entries';
 
 describe("Payload functional validation tests for V1 manual send", function() {
 
-  it("performs an XHR to /entries when Raygun.send() is called", function () {
+  it("performs an XHR to /entries when Raygun.send() is called", async function () {
     await browser.url('http://localhost:4567/fixtures/v1/manualSend.html');
 
     await browser.pause(6000);
@@ -16,64 +16,64 @@ describe("Payload functional validation tests for V1 manual send", function() {
       return window.__inFlightXHRs;
     });
 
-    var didPerformRequest = await _.any(inFlightXhrs, function (req) {
+    var didPerformRequest =  _.any(inFlightXhrs, function (req) {
       return req.url.indexOf(_entriesEndpoint) === 0;
     });
 
-    await expect(didPerformRequest).toBe(true);
+     expect(didPerformRequest).toBeTrue();
 });
 
-  it("doesn't performs an XHR to /entries when the API key isn't set", function () {
+  it("doesn't performs an XHR to /entries when the API key isn't set",async function () {
     await browser.url('http://localhost:4567/fixtures/v1/manualSendNoApiKey.html');
 
     await browser.pause(4000);
 
-    var inFlightXhrs = await browser.execute(function () {
+    var inFlightXhrs =  browser.execute(function () {
       return window.__inFlightXHRs;
     });
 
-    var didPerformRequest = await _.any(inFlightXhrs, function (req) {
+    var didPerformRequest =  _.any(inFlightXhrs, function (req) {
       return req.url.indexOf(_entriesEndpoint) === 0;
     });
 
-    await expect(didPerformRequest).toBe(false);
+     expect(didPerformRequest).toBeFalse();
   });
 
-  it("has the error message in the payload set", function () {
+  it("has the error message in the payload set", async function () {
     await browser.url('http://localhost:4567/fixtures/v1/manualSend.html');
 
     await browser.pause(4000);
 
-    var requestPayloads = await browser.execute(function () {
+    var requestPayloads =  browser.execute(function () {
       return window.__requestPayloads;
     });
 
-    var passes = await _.any(requestPayloads, function (payload) {
+    var passes =  _.any(requestPayloads, function (payload) {
       return payload.Details.Error.Message === 'Manual send' || payload.Details.Error.Message === 'Script error';
     });
 
-    await expect(passes).toBe(true);
+     expect(passes).toBeTrue();
   });
 
-  it("has the classname in the payload set", function () {
+  it("has the classname in the payload set",async function () {
     await browser.url('http://localhost:4567/fixtures/v1/manualSend.html');
 
     await browser.pause(4000);
 
-    var requestPayloads = await browser.execute(function () {
+    var requestPayloads =  browser.execute(function () {
       return window.__requestPayloads;
     });
 
-    var passes = await _.any(requestPayloads, function (payload) {
+    var passes =  _.any(requestPayloads, function (payload) {
       return payload.Details.Error.ClassName === 'Error';
     });
 
-    if (!(await common.isOldIE())) {
-      await expect(passes).toBe(true);
+    if (!( common.isOldIE())) {
+      await expect(passes).toBeTrue();
     }
   });
 
-  it("has the filename in the stacktrace payload set", function () {
+  it("has the filename in the stacktrace payload set",async function () {
     var pageUrl = 'http://localhost:4567/fixtures/v1/manualSend.html';
 
     await browser.url(pageUrl);
@@ -84,17 +84,17 @@ describe("Payload functional validation tests for V1 manual send", function() {
       return window.__requestPayloads;
     });
 
-    var passes = await _.any(requestPayloads, function (payload) {
+    var passes =  _.any(requestPayloads, function (payload) {
       var stackTrace = payload.Details.Error.StackTrace[0];
       return stackTrace && stackTrace.FileName === pageUrl;
     });
 
-    if (!(await common.isOldIE())) {
-      await expect(passes).toBe(true);
+    if (!( common.isOldIE())) {
+       expect(passes).toBeTrue();
     }
   });
 
-  it("has tags in the payload when tags are passed in", function () {
+  it("has tags in the payload when tags are passed in",async function () {
     var pageUrl = 'http://localhost:4567/fixtures/v1/manualSendTag.html';
 
     await browser.url(pageUrl);
@@ -105,14 +105,14 @@ describe("Payload functional validation tests for V1 manual send", function() {
       return window.__requestPayloads;
     });
 
-    var passes = await _.any(requestPayloads, function (payload) {
+    var passes =  _.any(requestPayloads, function (payload) {
       return payload.Details.Tags[0] === 'my_tag';
     });
 
-    await expect(passes).toBe(true);
+     expect(passes).toBeTrue();
   });
 
-  it("has custom data in the payload when custom data is passed in", function () {
+  it("has custom data in the payload when custom data is passed in",async function () {
     var pageUrl = 'http://localhost:4567/fixtures/v1/manualSendCustomData.html';
 
     await browser.url(pageUrl);
@@ -123,14 +123,14 @@ describe("Payload functional validation tests for V1 manual send", function() {
       return window.__requestPayloads;
     });
 
-    var passes = await _.any(requestPayloads, function (payload) {
+    var passes =  _.any(requestPayloads, function (payload) {
       return payload.Details.UserCustomData.myCustomKey === 'myCustomValue';
     });
 
-    await expect(passes).toBe(true);
+     expect(passes).toBeTrue();
   });
 
-  it("has correct user payload when Raygun.setUser() is called", function () {
+  it("has correct user payload when Raygun.setUser() is called", async function () {
     var pageUrl = 'http://localhost:4567/fixtures/v1/manualSendUser.html';
 
     await browser.url(pageUrl);
@@ -141,7 +141,7 @@ describe("Payload functional validation tests for V1 manual send", function() {
       return window.__requestPayloads;
     });
 
-    var passes = await _.any(requestPayloads, function (payload) {
+    var passes =  _.any(requestPayloads, function (payload) {
       return payload.Details.User.Identifier === 'user_email_address@localhost.local' &&
         payload.Details.User.IsAnonymous === false &&
         payload.Details.User.FirstName === 'Foo' &&
@@ -149,10 +149,10 @@ describe("Payload functional validation tests for V1 manual send", function() {
         payload.Details.User.UUID === 'BAE62917-ACE8-ab3D-9287-B6A33B8E8C55';
     });
 
-    await expect(passes).toBe(true);
+     expect(passes).toBeTrue();
   });
 
-  it("has correct version in payload when Raygun.setVersion() is called", function () {
+  it("has correct version in payload when Raygun.setVersion() is called",async function () {
     var pageUrl = 'http://localhost:4567/fixtures/v1/manualSendVersion.html';
 
     await browser.url(pageUrl);
@@ -163,11 +163,11 @@ describe("Payload functional validation tests for V1 manual send", function() {
       return window.__requestPayloads;
     });
 
-    var passes = await _.any(requestPayloads, function (payload) {
+    var passes =  _.any(requestPayloads, function (payload) {
       return payload.Details.Version === '1.0.0.0';
     });
 
-    await expect(passes).toBe(true);
+     expect(passes).toBeTrue();
   });
 
 });
