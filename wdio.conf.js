213cb3a4d41b03eabddf1eaa9bfc1c27498f72e2
Darcy Thomas <dthomas@raygun.com>
2023-07-11 16:25:56 +1200

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,7 +59,7 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'debug',
+    logLevel: 'info',
     //
     // Set specific log levels per logger
     // loggers:
