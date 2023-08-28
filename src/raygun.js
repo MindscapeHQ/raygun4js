1512d8f7147f8585b4d32b0b91c9f8f4af7340e3
Darcy Thomas <dthomas@raygun.com>
2023-08-28 13:10:33 +1200

    Update raygun.js

--- src/raygun.js
+++ src/raygun.js
@@ -557,7 +557,7 @@ var raygunFactory = function (window, $, undefined) {
     _processExceptionQueue = [];
 
     for (i = 0; i < _trackEventQueue.length; i++) {
-      _publicRaygunFunctions.trackEvent(_trackEventQueue[i].type, _trackEventQueue[i].options);
+      _publicRaygunFunctions.trackEvent(_trackEventQueue[i].type, _trackEventQueue[i].options , _trackEventQueue[i].parentResource || null);
     }
 
     _trackEventQueue = [];
