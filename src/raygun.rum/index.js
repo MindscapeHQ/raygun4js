9f1d2bcb24e5f6d6a7621afbdf4b9e46d49e9279
hamish taylor <hamish@raygun.com>
2023-09-18 15:42:08 +1200

    Fixed number NaN check

--- src/raygun.rum/index.js
+++ src/raygun.rum/index.js
@@ -1206,7 +1206,7 @@ var raygunRumFactory = function (window, $, Raygun) {
 
     function sanitizeNaNs(data) {
       for (var i in data) {
-        if (data[i] === "NaN" || data[i] === NaN) {
+        if (data[i] === "NaN" || Number.isNaN(data[i])) {
           data[i] = 0;
         }
       }
